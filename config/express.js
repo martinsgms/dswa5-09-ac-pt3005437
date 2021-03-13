13bb62dd8e439f34b8bda3f2db5818c30d67e8e4
Gabriel Martins dos Santos <gabriel.martins.dos.santos@ericsson.com>
2021-03-13 17:16:16 -0300

    corrige porta no arquivo express

--- config/express.js
+++ config/express.js
@@ -3,7 +3,7 @@ var home = require('../app/routes/home');
 
 module.exports = () => {
     var app = express();
-    app.set('port', 3000);
+    app.set('port', process.env.PORT);
     
     app.use(express.static('./public'));
     app.set('view engine', 'ejs');

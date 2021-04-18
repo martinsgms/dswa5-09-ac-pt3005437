346fa1fe2af34cef088b6348422b40298e7d027d
Gabriel Martins dos Santos <martins.gabriel@aluno.ifsp.edu.br>
2021-04-18 20:09:41 -0300

    adiciona listagem de contatos

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -4,5 +4,13 @@ ngApp.controller('ContatosController', function($scope) {
         $scope.incrementa = () => {
             $scope.total++;
         }
+
+        $scope.contatos =  [
+            { _id: 1, nome: 'Gabriel Martins', email: 'gabriel.martins@ifsp.edu.br' },
+            { _id: 2, nome: 'Maria Aparecida', email: 'maria.aparecida@ifsp.edu.br' },
+            { _id: 3, nome: 'João Teixeira', email: 'joao.teixeira@ifsp.edu.br' }
+        ]
+
+        $scope.filtro = '';
     }
 )

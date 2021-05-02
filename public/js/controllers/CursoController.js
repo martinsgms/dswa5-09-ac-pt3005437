ea4dd97174f7bcd399cd0d2104d934a8e645625a
Gabriel Martins dos Santos <martins.gabriel@aluno.ifsp.edu.br>
2021-05-02 15:10:24 -0300

    cria tela de detalhes do curso

---
+++ public/js/controllers/CursoController.js
@@ -0,0 +1,16 @@
+ngApp.controller('CursoController', function($scope, $routeParams, $resource) {
+    
+    var Curso = $resource('/cursos/:id');
+    Curso.get({ id: $routeParams.id },
+        function(curso) {
+            $scope.curso = curso;
+        },
+        function(erro) {
+            $scope.mensagem = {
+                texto: 'Não foi possível obter o curso.'
+            };
+            console.log($routeParams.id);
+        }
+    );
+
+})
--- public/js/main.js
+++ public/js/main.js
@@ -11,14 +11,19 @@ ngApp.config($routeProvider => {
         controller : 'ContatosController'
     });
 
+    $routeProvider.when('/contato/:id', {
+        templateUrl : '/partials/contato.html',
+        controller : 'ContatoController'
+    });
+
     $routeProvider.when('/cursos', {
         templateUrl : '/partials/cursos.html',
         controller : 'CursosController'
     });
 
-    $routeProvider.when('/contato/:id', {
-        templateUrl : '/partials/contato.html',
-        controller : 'ContatoController'
+    $routeProvider.when('/curso/:id', {
+        templateUrl : '/partials/curso.html',
+        controller : 'CursoController'
     });
 
     $routeProvider.otherwise({redirectTo: '/contatos'})
